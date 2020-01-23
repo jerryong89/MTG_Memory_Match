840130317af7c99842686e4bbd79e884eb6bb513
Jerry Vinh Ong <jerryong89@gmail.com>
2020-01-22 16:42:47 -0800

    flipping-cards - jerry ong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-
+var container = document.getElementById("gameCards")
+container.addEventListener("click",
+  function handleClick(event) {
+    if (event.target.className.indexOf("card-back") === -1) {
+      return;
+    }
+    var divElement = event.target;
+    divElement.className +="hidden";
+    console.log(event)
+  }
+);
