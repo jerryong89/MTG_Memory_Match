83925aa399bac686a1df032dab52c48aac810cc4
Jerry Vinh Ong <jerryong89@gmail.com>
2020-01-23 23:27:46 -0800

    match two cards - jerry ong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
-var container = document.getElementById("gameCards")
-container.addEventListener("click",
-  function handleClick(event) {
-    if (event.target.className.indexOf("card-back") === -1) {
-      return;
-    }
-    var divElement = event.target;
-    divElement.className +="hidden";
-    console.log(event)
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
+
+var container = document.getElementById("gameCards");
+container.addEventListener("click", handleClick);
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
   }
-);
+  var divElement = event.target;
+  divElement.className += " hidden";
+  if (!firstCardClicked) {
+    firstCardClicked = divElement;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = divElement;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    container.removeEventListener("click", handleClick);
+    if (firstCardClasses === secondCardClasses) {
+      container.addEventListener("click", handleClick);
+      firstCardClicked = !firstCardClicked;
+      secondCardClicked = !secondCardClicked;
+    } else {
+      setTimeout(function () {
+        firstCardClicked.classList.remove("hidden"),
+          secondCardClicked.classList.remove("hidden"),
+          firstCardClicked = !firstCardClicked,
+          secondCardClicked = !secondCardClicked,
+          container.addEventListener("click", handleClick)
+      }, 1500);
+    }
+  }
+}
